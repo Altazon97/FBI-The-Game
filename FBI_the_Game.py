b6b2ef0badae44724cae69e7cb99fb4a7ca9ce0e
Andy Zeng <andyz@sfu.ca>
2015-11-25 11:46:04 -0800

    Full Functionality
    Finished isCompleteBone( ) function. 
    
    TODO: 
    1. Our    Try; except ValueError   check in the data file input doesn't work; program still crashes when we enter wrong filename 
        --> We can either take this out entirely (it's not required) or work on fixing this. 
    2. We need to try more complex datasets to test for the scalability 
    3. There is a commented out region for debugging within the isCompleteBone( ) function. 
        --> We can take that out after we test on bigger datasets 
    4. General presentation and annotations - do we need student numbers?

--- FBI_the_Game.py
+++ FBI_the_Game.py
@@ -19,17 +19,16 @@ def getDataFile():
     """This function asks the user to enter the name of the data file and reads it.
     If it cant be read or doesn't exist, the user is asked to try again.
     """
-
+    
     #Open the file before doing anything
     getfile = 0
     while getfile == 0:
-        dataFile = input("Please enter the name of the data file: ")    ### FOR DEBUGGING
-        #dataFile = input("Please, enter the name of the file with the data you wish to use: ")
+        dataFile = input("Please, enter the name the data file: ")
         try:
             data = open(dataFile, 'r')
             getfile = 1
         except ValueError:
-            print("You have entered an incorrect filename, please try again.\n")
+            print("\nYou have entered an incorrect filename, please try again.\n")
     return data
 
 
@@ -37,7 +36,7 @@ def getDataFile():
 def getCoordinates():
     """This function gets the coordinates of bones from data file.
     """
-
+    
     eachLine = data.readlines()
     allCoordinates = []
     #Iterate through lines
@@ -59,7 +58,7 @@ def getCoordinates():
 def isHit():
     """This function returns True if the inputed coordinates has hit part of a bone.
     """
-
+    
     for i in range(numberOfBones):  #Do the following for all the bones...
         line = bonelist[i]
         for j in range(sizeOfBone): #Check if coordinate matches a particular bone
@@ -69,14 +68,75 @@ def isHit():
                 return True
 
 
+#Check if user has dug up an entire bone
+def isCompleteBone(row, col):
+    """This function takes a coordinate and checks its surroundings ((sizeOfBone - 1) positions in all four directions)
+    for positions where the user has revealed "B". It then looks within all these positions to see whether the user has fully revealed a bone
+    """
+    
+    position = [row,col]            #initialize your position with your last guest
+    coordinate_list = [position]    #list of all coordinates within (sizeOfBone - 1) of your selected position, starts with your position
+    hit_list = []                   #list of all coordinates that contain "B" within coordinate_list
+    new_hit_list = []               #list of all coordinates within hit_list that match a specific bone
+    bone = []
+    
+    #Save all coordinates (sizeOfBone - 1) above your selected position
+    for i in range(sizeOfBone):
+        position = [row - i, col]
+        if position[0] != row and position[0] >= 0:     #if not your position and is within boundaries
+            coordinate_list.append(position)
+    #Save all coordinates (sizeOfBone - 1) below your selected position
+    for i in range(sizeOfBone):
+        position = [row + i, col]
+        if position[0] != row and position[0] < xAxis:  #if not your position and is within boundaries
+            coordinate_list.append(position)
+    #Save all coordinates (sizeOfBone - 1) left of your selected position
+    for i in range(sizeOfBone):
+        position = [row, col - i]
+        if position[1] != col and position[1] >= 0:     #if not your position and is within boundaries
+            coordinate_list.append(position)
+    #Save all coordinates (sizeOfBone - 1) right of your selected position
+    for i in range(sizeOfBone):
+        position = [row, col + i]
+        if position[1] != col and position[1] < yAxis:  #if not your position and is within boundaries
+            coordinate_list.append(position)
+            
+    #Create hit_list with only valid hits from coordinate_list
+    for pos in coordinate_list:
+        if yard[pos[0]][pos[1]] == "B":
+            hit_list.append(pos)
+    #Iterate through each bone from your data
+    for i in range(numberOfBones):
+        #Change the bone coordinates from strings to integers
+        for pos in bonelist[i]:
+            pos = [int(pos[0]),int(pos[1])]
+            bone.append(pos)
+        #Take all positions from hit_list that are found in current bone and make new_hit_list
+        for pos in hit_list:
+            if pos in bone:
+                new_hit_list.append(pos)
+                
+##        #FOR DEBUGGING
+##        print(sorted(bone), "bone \n")
+##        print(sorted(new_hit_list), "new_hit_list \n")
+                
+        #Sort and compare bone and new_hit_list to see if they are equivalent
+        if sorted(bone)== sorted(new_hit_list):
+            print("\nYou have fully uncovered bone " + str(i) + ". Great Job!")
+
+        #Clear bone and new_hit_list to iterate through next bone
+        bone = []
+        new_hit_list = []
+        
+
 #Check to see if user has revealed all bones
-def isComplete():
+def isCompleteYard():
     """This function generates a yard with all of the bones displayed, compares it to the user-generated yard.
     If the two are the same (i.e. user is done the game), prints out the yard and returns True.
     """
-
+    
     #Create complete yard to compare
-    complete_yard = [["." for x in range(int(xAxis))] for x in range(int(yAxis))]   #new matric for the complete yard
+    complete_yard = [["." for x in range(int(xAxis))] for x in range(int(yAxis))]   #new matrix for the complete yard
     for i in range(numberOfBones):  #Do the following for all the bones...
         line = bonelist[i]
         for j in range(sizeOfBone): #Check if coordinate matches a particular bone
@@ -85,21 +145,24 @@ def isComplete():
             col = int(pos[1])
             #Assign each B to complete yard
             complete_yard[row][col] = "B"
-
+            
     #Check if user yard is complete
     if yard == complete_yard:
         print("\n")         #Separator for aesthetic effect
         showYard()          #Display completed yard
         return 1
-
-
+    
+    
 #Reveal the bone if user guess correctly
 def revealBone():
     """This function changes a "." to a "B" in the yard matrix when a bone is hit.
     """
-
+    
     if isHit():
+        #Replace the "." with "B"
         yard[int(guess[0])][int(guess[1])] = "B"
+        #Check if the user has dug up an entire bone
+        isCompleteBone(int(guess[0]),int(guess[1]))
     else:
         print("\n****OOPS! MISSED!****")
 
@@ -128,20 +191,22 @@ def showYard():
 def isNotInteger():
     """This function checks to see whether the input is an integer or not.
     """
-
+    
     try:
         guess[0] = int(guess[0])
     except ValueError:
         return True
+    
 
 #=============================== End Defining Functions ===============================#
 
 
 #================================= Start Main Program =================================#
 
+
 #Introduce the User
 print("""Welcome to Fast Bone Investigation (FBI) the game.
-In this game, we dig out bones from Mrs. Hudson's backyard!\n""")
+In this game, we dig out bones from Mrs. Hudson's backyard!""")
 
 #Get the data file
 data = getDataFile()
@@ -169,7 +234,7 @@ while running:
 (e.g., 0 3 if you suspect that part of a bone is buried on the first row at the 4th column).
 Enter -1 to quit : """).split()
 
-    #Check for empty input
+    #Check for empty input 
     if len(guess) == 0:
         print("***You have not entered anything. You need to enter a valid row and the column number!")
     #Check to ensure input is integer
@@ -179,7 +244,7 @@ Enter -1 to quit : """).split()
     elif int(guess[0]) == -1:
         print("\nWasn't it fun! Bye!")
         running = False
-    #If the user doesn't enter both row and column values
+    #If the user doesn't enter both row and column values  
     elif len(guess) == 1:
         print("***You have entered only 1 value. You need to enter a valid row and the column number!")
     #If coordinate is beyond the backyard's dimensions
@@ -190,13 +255,9 @@ Enter -1 to quit : """).split()
         #See if user guess was correct
         revealBone()
         #If they have completed the game
-        if isComplete():
-            print("Congratulations! You have found all the bones.")
+        if isCompleteYard():
+            print("\nCongratulations! You have found all the bones. Goodbye!\n")
             running = False
 
 
 #================================== End Main Program ==================================#
-
-"""
-- code to say if entire bone is hit
-"""
